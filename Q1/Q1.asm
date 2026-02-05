68c217dba007cfbd8face45fed07f922495b1b3b
dhanish0711 <dhanishladwani@gmail.com>
2026-02-05 15:16:55 +0530

    Add 8051 Assembly program for PRN last four digits
    Implement 8051 Assembly program to calculate last four digits of PRN using arithmetic operations.

--- Q1/Q1.asm
+++ Q1/Q1.asm
@@ -1,3 +1,4 @@
+; Q1. Write an 8051 Assembly Language Program (ALP) to generate the last four digits of your PRN using any arithmetic instructions. The program should not directly load the complete PRN number as an immediate value. Instead, it must use appropriate arithmetic operations such as ADD, MUL, or INC to form the number logically. The final result must be stored in the Accumulator register (AX). For example, if a student’s PRN is 24070521211, the last four digits are 1211, and the value 1211 should be available in AX at the end of program execution. 
 ORG 0000H
 
 MOV A,#0AH     ; 0AH = 10
